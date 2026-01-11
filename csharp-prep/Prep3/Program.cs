becfec82890d1817fe91d780298d07cab7872ab6
derekgelveza <gel24002@byui.edu>
2026-01-11 16:20:11 -0700

    gave replay option

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,30 +5,45 @@ class Program
     static void Main(string[] args)
     {
         Random randomGenerator = new Random();
-        int luckyNumber = randomGenerator.Next(1, 100);
         int userGuess;
+        string answer;
 
-        int attempts = 0;
-
+  
 
         do
         {
-            Console.Write("What is your magic number? "); 
-            string userResponse = Console.ReadLine();
-            userGuess = int.Parse(userResponse);
-            attempts++;
 
-            if (userGuess > luckyNumber)
+            int luckyNumber = randomGenerator.Next(1, 100);
+            int attempts = 0;
+
+            do
             {
-                Console.WriteLine("lower");
-            }
-            else if (userGuess < luckyNumber)
-            {
-                Console.WriteLine("higher");
-            }
-        } while (userGuess != luckyNumber);
-        Console.WriteLine("You guessed it!");
-        Console.WriteLine($"It took you {attempts} tries.");
+                Console.Write("What is your magic number? "); 
+                string userResponse = Console.ReadLine();
+                userGuess = int.Parse(userResponse);
+                attempts++;
+
+                if (userGuess > luckyNumber)
+                {
+                    Console.WriteLine("lower");
+                }
+                else if (userGuess < luckyNumber)
+                {
+                    Console.WriteLine("higher");
+                }
+            } while (userGuess != luckyNumber);
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"It took you {attempts} tries.");
+
+            Console.WriteLine("Would you like to play again?");
+            answer = Console.ReadLine();
+
+        } while (answer == "yes" || answer == "Yes");
+        if (answer == "no" || answer == "No")
+        {
+            Console.Write("Ok, have a good day!");
+        }
+        
 
 
 
