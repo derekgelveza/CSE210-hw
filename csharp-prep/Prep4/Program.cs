b9bfd8f95d43d2654c27e63ea7e3cd1f67eae288
derekgelveza <gel24002@byui.edu>
2026-01-12 13:44:43 -0700

    list assignment done

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> userNumbers = new List<int>();
+        int answer;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        Console.Write("Enter a number: ");
+        string userInput = Console.ReadLine();
+        answer = int.Parse(userInput);
+        do
+        {
+            if (answer != 0)
+            {
+               userNumbers.Add(answer);
+            }
+            Console.Write("Enter a number: ");
+            userInput = Console.ReadLine();
+            answer = int.Parse(userInput);
+
+        }while (answer != 0);
+        {
+            double average = userNumbers.Average();
+            int max = userNumbers.Max();
+            
+
+            Console.WriteLine($"Your average is: {average}.");
+            Console.WriteLine($"The largest number is: {max}.");
+        }
     }
 }
